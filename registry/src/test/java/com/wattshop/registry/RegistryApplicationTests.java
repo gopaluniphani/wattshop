6f6407385e6f0318d087b109a26052207cbb4a15
Phani Gopaluni <gopaluniphani@gmail.com>
2021-01-14 15:56:41 +0530

    Added API Gateway and Service Discovery

---
+++ registry/src/test/java/com/wattshop/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wattshop.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
